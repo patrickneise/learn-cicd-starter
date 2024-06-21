add70fba08654fccdbb4c82aa245c953ceb27b37
patrickneise <patrick.neise@gmail.com>
2024-06-21 18:40:40 -0400

    add tests for auth.GetAPIKey

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,21 +2,23 @@ package auth
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 )
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
+var ErrMalformedAuthorizationHeader = errors.New("malformed authorization header")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "", ErrNoAuthHeaderIncluded
+		return "", fmt.Errorf("%q: %w", headers, ErrNoAuthHeaderIncluded)
 	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("malformed authorization header")
+		return "", fmt.Errorf("%q: %w", headers, ErrMalformedAuthorizationHeader)
 	}
 
 	return splitAuth[1], nil
